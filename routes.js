e8fdcca279d5cb51f30d8583196935cc84b54f55
Joao Victor <joaovictorcostasouza15@gmail.com>
2020-08-06 18:34:06 -0300

    Rotas

--- routes.js
+++ routes.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/', (req,res)=>{
-
-    return res.send('Olá pessoal estou funcionando')
-
+routes.get('/', (req, res) => {
+    return res.redirect('/Members')
+})
+routes.get('/Members', (req, res) => {
+    return res.render('Members/index')
+})
+routes.get('/Instructors', (req, res) => {
+    return res.render('Instructors/index')
 })
 
 module.exports = routes
