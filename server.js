23d769cdb1e6ce86b941301d00e007a9077e9787
Joao Victor <joaovictorcostasouza15@gmail.com>
2020-08-07 18:24:05 -0300

    Adicionando metodos que permite usar o req.body

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const nunjucks = require('nunjucks')
 const routes  = require('./routes')
 
 const server = express()
-
+ 
+server.use(express.urlencoded({extended: true})) //faz com que podemossemos usar o metodo req.body
+//para fazer o array com os itens dos campos.
 server.use(express.static('public')) //arrumando a pasta public
 server.use(routes) // Criando rotas
 server.set('view engine', 'njk') //Configurando a view engine
