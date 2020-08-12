e333907ee4843aae6f8118d35dd7895fdd28095f
JoaoSouza0 <jooavictorcostasouza15@gmail.com>
2020-08-12 07:50:49 -0300

    criando função para buscar instrutores e membros

--- logic/logic.js
+++ logic/logic.js
@@ -1,6 +1,8 @@
+const data = require('../data.json')
+
 module.exports = {
 
-    calcularIdade :function (timestamp) {
+    calcularIdade: function (timestamp) {
         const today = new Date
         const dataNascimento = new Date(timestamp)
 
@@ -12,15 +14,49 @@ module.exports = {
         }
         return age
     },
-    calcularData: function(timestamp){
+    calcularData: function (timestamp) {
         const date = new Date(timestamp)
 
         const year = date.getUTCFullYear()
-        const month= `0${date.getUTCMonth()+1.}`.slice(-2)
-        const Day= `0${date.getUTCDate()}`.slice(-2)
+        const month = `0${date.getUTCMonth() + 1.}`.slice(-2)
+        const Day = `0${date.getUTCDate()}`.slice(-2)
 
-        return{ 
-            birthDay:`${Day}/${month}`,
-            iso:`${year}-${month}-${Day}`}
+        return {
+            birthDay: `${Day}/${month}`,
+            iso: `${year}-${month}-${Day}`
+        }
+    },
+    searchInstructor(id, res) {
+
+        let index = 0
+        const encontrarEstrutor = data.instructors.find(function (instructor,foundIndex) { //função que vai validar o instrutor   
+            // função find para percoorer os dados a procura dele
+            
+          if (id == instructor.id)//validando se é o id certo passado no parametro a cima
+          {
+              index = foundIndex
+              return true
+          }
+        })
+
+        if (!encontrarEstrutor) return res.send('Não encontrei')
+
+        return { encontrarEstrutor, index }
+    },
+    searchMembers(id, res) {
+
+        let index = 0
+        const searchMember = data.members.find((member,foundIndex) => { //função que vai validar o instrutor   
+            // função find para percoorer os dados a procura dele
+          if (id == member.id)//validando se é o id certo passado no parametro a cima
+          {
+              index = foundIndex
+              return true
+          }
+        })
+
+        if (!searchMember) return res.send('Não encontrei')
+
+        return { searchMember, index }
     }
 }
